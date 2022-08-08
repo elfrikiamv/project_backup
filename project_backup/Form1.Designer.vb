2200f93ae152aad50fa4a9eff2734831a58c76ea
elfrikiamv <elfrikiamv@gmail.com>
2022-08-08 18:42:08 -0500

    save: detect or create dir for copy

--- project_backup/Form1.Designer.vb
+++ project_backup/Form1.Designer.vb
@@ -28,7 +28,10 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "project_backup"
         Me.ResumeLayout(False)
--- project_backup/Form1.vb
+++ project_backup/Form1.vb
@@ -1,3 +1,20 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim project_backup_patch As String = "F:\project_backup\" & DateString & "\big_data\projects"
+
+        'verifica si ya existe una carpeta con la fecha actual para hacer la copia
+        If Directory.Exists(project_backup_patch) Then
+
+            My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", "F:\project_backup\" & DateString & "\big_data\projects", True)
+        Else
+
+            'crea la carpeta con la fecha y hace la copia
+            My.Computer.FileSystem.CreateDirectory("F:\project_backup\" & DateString & "\big_data\projects")
+            My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", "F:\project_backup\" & DateString & "\big_data\projects", True)
+        End If
+    End Sub
 End Class
