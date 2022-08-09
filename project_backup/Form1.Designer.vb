e2932f06937075de30f761aab433b9f3c3ec3b5c
elfrikiamv <elfrikiamv@gmail.com>
2022-08-09 17:13:50 -0500

    rename mkdr by date and time, desactivate ContextSwitchDeadlock

--- project_backup/Form1.Designer.vb
+++ project_backup/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.SuspendLayout()
         '
         'Form1
@@ -31,6 +32,7 @@ Partial Class Form1
         Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "project_backup"
--- project_backup/Form1.vb
+++ project_backup/Form1.vb
@@ -4,22 +4,36 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim project_backup_patch As String = "F:\project_backup\" & DateString & "\big_data\projects"
+        'set name dir "date and time"
+        Dim mkdir As String = "date-" & DateString & "-time-" & TimeString.Replace(":", "-")
 
-        'verifica si ya existe una carpeta con la fecha actual para hacer la copia
+        Dim project_backup_patch As String = "F:\project_backup\" & mkdir & "\big_data\projects"
+
+        'verifica si ya existe una carpeta con la fecha y hora actual
         If Directory.Exists(project_backup_patch) Then
 
-            My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", "F:\project_backup\" & DateString & "\big_data\projects", True)
+            Dim dirproject_backup_true As String
+            dirproject_backup_true = MsgBox("¡Ya tienes una copia!" & vbNewLine & "¿Quieres remplazarla para continuar?", vbYesNo)
+
+            If (dirproject_backup_true = vbYes) Then
+
+                'delete project_backup_patch and new copy
+                Directory.Delete(project_backup_patch, True)
+                My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", project_backup_patch, True)
+            Else
+                End
+            End If
         Else
 
-            'crea la carpeta con la fecha y hace la copia
-            My.Computer.FileSystem.CreateDirectory("F:\project_backup\" & DateString & "\big_data\projects")
-            My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", "F:\project_backup\" & DateString & "\big_data\projects", True)
+            'crea la carpeta con la fecha y hora, para hacer la copia
+            My.Computer.FileSystem.CreateDirectory("F:\project_backup\" & mkdir & "\big_data\projects")
+            My.Computer.FileSystem.CopyDirectory("C:\big_data\projects", project_backup_patch, True)
+
+            'msg de copia realizada
+            MsgBox("Ya quedo!", MsgBoxStyle.Information, "project_backup")
+
+            End
+
         End If
-
-        MsgBox("Ya quedo!", MsgBoxStyle.Information, "#")
-
-        End
-
     End Sub
 End Class
